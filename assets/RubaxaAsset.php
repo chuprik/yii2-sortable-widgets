49e9108999ff004ddc4fc116ba9b325b5637cb83
Constantine Chuprik <constantinchuprik@gmail.com>
2015-08-10 12:14:24 +0600

    drop bower using;

--- assets/RubaxaAsset.php
+++ assets/RubaxaAsset.php
@@ -6,11 +6,11 @@ use yii\web\AssetBundle;
 
 class RubaxaAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/Sortable';
+    public $baseUrl = '@vendor/kotchuprik/yii2-sortable-widgets/assets/files';
 
     public $js = [
-        'Sortable.js',
-        'jquery.binding.js',
+        '//cdnjs.cloudflare.com/ajax/libs/Sortable/1.2.1/Sortable.js',
+        'js/jquery.binding.js',
     ];
 
     public $depends = [
